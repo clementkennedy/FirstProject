58dca3ec91a8df1ac2d16e10f4c8661d1f43cff6
clementkennedy <clementkennedya@gmail.com>
2022-01-29 01:18:47 +0530

    this is first commit

---
+++ AddNumbers.java
@@ -0,0 +1,12 @@
+class Add {
+    public  static void main (String [] args) {
+        int a = 5;
+        int b = 5;
+        int c = a +b;
+        System.out.println("The answer is = "+c);
+        
+    }
+} 
+
+
+
