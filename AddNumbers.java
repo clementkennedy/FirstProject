6a951cc711e4f2b3d1c42cee20d5099cf09ea9b2
clementkennedy <98611311+clementkennedy@users.noreply.github.com>
2022-01-29 15:36:47 +0530

    change b to 50

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,7 +1,7 @@
 class Add {
     public  static void main (String [] args) {
         int a = 5;
-        int b = 5;
+        int b = 50;
         int c = a +b;
         System.out.println("The answer is = "+c);
         
